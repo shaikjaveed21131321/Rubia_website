e8f8e05648c5b1b04745de290da0ab591c0bbad7
rvxx007 <akashkawle0@gmail.com>
2024-07-23 18:28:43 +0530

    taxi page form design changes happen

--- src/components/taxi/taxi.jsx
+++ src/components/taxi/taxi.jsx
@@ -1,25 +1,88 @@
-import React, { useState } from "react";
-import Select from "react-select";
+import React, { useState } from 'react';
+import swal from "sweetalert";
 
 // import Banner9 from "../assets/Group 91.jpg"
 const Taxi = () => {
-  const [selectedOption, setSelectedOption] = useState(null);
-  const options1 = [
-    { value: "Airport", label: "Airport" },
-    { value: "Local", label: "Local" },
-    { value: "Out Station", label: "Out Station" },
-  ];
-  const options2 = [
-    { value: "One-Way Trip ", label: "One-Way Trip " },
-    { value: "Round Trip", label: "Round Trip" },
+  const [formValues, setFormValues] = useState({
+    tx_type:'',
+    tx_trip_type:'',
+    tx_return_type:'',
+    tx_fname:'',
+    tx_lname:'',
+    tx_pick_add:'',
+    tx_drop_add:'',
+    tx_pick_date:'',
+    tx_drop_date:'',
+    tx_pick_time:'',
+    tx_drop_time:'',
+    tx_mob_num:'',
+    tx_email:'',
+    tx_comm:'',
+    created_at:''
+  })
+ 
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormValues({
+      ...formValues,
+      [name]: value,
+    });
+  };
 
-  ];
-  const options3 = [
-    { value: "Hatch back", label: "Hatch back" },
-    { value: "Sedan", label: "Sedan" },
-    { value: "SUV", label: "SUV" },
-    {value:"Luxury", label:"Luxury"}
-  ];
+  const handelSubmit = (e) => {
+    e.preventDefault();
+    // if (!validate()) return;
+
+    let data = JSON.stringify(
+      {
+        tx_type:formValues.tx_type.trim(),
+        tx_trip_type:formValues.tx_trip_typetrim(),
+        tx_return_type:formValues.tx_return_typetrim(),
+        tx_fname:formValues.tx_fnametrim(),
+        tx_lname:formValues.tx_lnametrim(),
+        tx_pick_add:formValues.tx_pick_addtrim(),
+        tx_drop_add:formValues.tx_drop_addtrim(),
+        tx_pick_date:formValues.tx_pick_datetrim(),
+        tx_drop_date:formValues.tx_drop_datetrim(),
+        tx_pick_time:formValues.tx_pick_timetrim(),
+        tx_drop_time:formValues.tx_drop_timetrim(),
+        tx_mob_num:formValues.tx_mob_numtrim(),
+        tx_email:formValues.tx_emailtrim(),
+        tx_comm:formValues.tx_commtrim(),
+        created_at:new Date(),
+       
+      }
+    )
+
+    console.log(data);
+    let config = {
+      method: "POST",
+      maxBodyLength: Infinity,
+      body: data,
+      redirect: "follow",
+      headers: {
+        'content-Type': 'application/json'
+      },
+
+    };
+
+    fetch("http://192.168.0.114:4000/api/v1/taxi/register/", config)
+      .then((response) => response.json())
+      .then((result) => {
+        if (result.success === true) {
+          //coustom  message Altert box 
+          swal("successful Registred", result.message, "success");
+          // alert(result.message)
+        }
+        else {
+
+          swal("Error", result.message, "error")
+          // alert(result.message)
+        }
+      }).catch((error) => swal("Error", error, "error"));
+
+  }
+ 
   return (
     <>
       <div className="taxi-text">
@@ -33,41 +96,60 @@ const Taxi = () => {
             <div className="car_taxi">
               
               <div className="form" style={{ margin: "auto" }}>
-                <form action="">
+                <form action="" onSubmit={{handelSubmit}}>
                 
                 <div className="buttons">
-                <Select
-                  defaultValue={selectedOption}
-                  onChange={setSelectedOption}
+                {/* <Select
+                  defaultValue={setTx_type}
+                  onChange={setTx_type}
                   options={options1}
                   className="dropdown_taxi"
-                  placeholder="Airport "
+                  placeholder="Airport"
                 />
                 <Select
-                  defaultValue={selectedOption}
-                  onChange={setSelectedOption}
+                  defaultValue={setTx_trip_type}
+                  onChange={setTx_trip_type}
                   options={options2}
                    className="dropdown_taxi"
                    placeholder="One-Way Trip "
                 />
                 <Select
-                  defaultValue={selectedOption}
-                  onChange={setSelectedOption}
+                  defaultValue={setTx_return_type}
+                  onChange={setTx_return_type}
                   options={options3}
                   className="dropdown_taxi"
                   placeholder="Hatch back"
-                />
+                /> */}
+  
+                <select onChange={handleChange} style={{background:"#FFF",padding:"5px", fontWeight:"600" , border:"1px solid #FFA3A3"}} name="tx_type" id="" className="dropdown_taxi">
+                  <option  value="Airport">Airport</option>
+                  <option value="Local">Local</option>
+                  <option value="Out Station">Out Station</option>
+                </select>
+                
+                <select onChange={handleChange} style={{background:"#FFF",padding:"5px", fontWeight:"600" , border:"1px solid #FFA3A3"}} name="tx_trip_type" id="" className="dropdown_taxi">
+                  <option value="One-Way Trip">One-Way Trip</option>
+                  <option value="Round Trip">Round Trip</option>
+                </select>
+       
+                <select onChange={handleChange} style={{background:"#FFF",padding:"5px", fontWeight:"600" , border:"1px solid #FFA3A3"}} name="tx_return_type" id="" className="dropdown_taxi">
+                  <option value="Hatch back">Hatch back</option>
+                  <option value="Sedan">Sedan</option>
+                  <option value="SUV">SUV</option>
+                  <option value="Luxury">Luxury</option>
+                </select>
               </div>
                 <div  className="main_form">
-
                   <div>
                     <label className="label_taxi" for="input1">
                       frist name
                     </label>
                     <input
                       type="text"
+                      onChange={handleChange}
+                      style={{background:"#FFF",padding:"5px", fontWeight:"600" , border:"1px solid #FFA3A3"}}
                       id="input1"
-                      name="fname"
+                      name="tx_fname"
                       placeholder="Enter Frist Name"
                     />
                   </div>
@@ -77,8 +159,10 @@ const Taxi = () => {
                     </label>
                     <input
                       type="text"
+                      onChange={handleChange}
+                      style={{background:"#FFF",padding:"5px", fontWeight:"600" , border:"1px solid #FFA3A3"}}
                       id="input2"
-                      name="input2"
+                      name="tx_lname"
                       placeholder="Enter Last Name"
                     />
                   </div>
@@ -88,8 +172,10 @@ const Taxi = () => {
                     </label>
                     <input
                       type="text"
+                      onChange={handleChange}
+                      style={{background:"#FFF",padding:"5px", fontWeight:"600" , border:"1px solid #FFA3A3"}}
                       id="input3"
-                      name="input3"
+                      name="tx_drop_add"
                       placeholder="Enter Drop Address"
                     />
                   </div>
@@ -99,8 +185,10 @@ const Taxi = () => {
                     </label>
                     <input
                       type="text"
+                      onChange={handleChange}
+                      style={{background:"#FFF",padding:"5px", fontWeight:"600" , border:"1px solid #FFA3A3"}}
                       id="input4"
-                      name="input4"
+                      name="tx_pick_add"
                       placeholder="Enter Pickup Address"
                     />
                   </div>
@@ -108,25 +196,33 @@ const Taxi = () => {
                     <label className="label_taxi" for="input5">
                       pickup date
                     </label>
-                    <input type="date" id="input5" name="input5" />
+                    <input type="date"
+                    onChange={handleChange}
+                    style={{background:"#FFF",padding:"5px", fontWeight:"600" , border:"1px solid #FFA3A3"}} id="input5" name="tx_pick_date" />
                   </div>
                   <div>
                     <label className="label_taxi" for="input6">
                       drop date
                     </label>
-                    <input type="date" id="input6" name="input6" />
+                    <input type="date"
+                    onChange={handleChange}
+                    style={{background:"#FFF",padding:"5px", fontWeight:"600" , border:"1px solid #FFA3A3"}} id="input6" name="tx_drop_date" />
                   </div>
                   <div>
                     <label className="label_taxi" for="input7">
                       pickup time
                     </label>
-                    <input type="time" id="input7" name="input7" />
+                    <input type="time"
+                    onChange={handleChange}
+                    style={{background:"#FFF",padding:"5px", fontWeight:"600" , border:"1px solid #FFA3A3"}} id="input7" name="tx_pick_time" />
                   </div>
                   <div>
                     <label className="label_taxi" for="input8">
                       drop time
                     </label>
-                    <input type="time" id="input8" name="input8" />
+                    <input type="time"
+                    onChange={handleChange}
+                    style={{background:"#FFF",padding:"5px", fontWeight:"600" , border:"1px solid #FFA3A3"}} id="input8" name="tx_drop_time" />
                   </div>
                   <div>
                     <label className="label_taxi" for="input9">
@@ -134,8 +230,10 @@ const Taxi = () => {
                     </label>
                     <input
                       type="text"
+                      onChange={handleChange}
+                      style={{background:"#FFF",padding:"5px", fontWeight:"600" , border:"1px solid #FFA3A3"}}
                       id="input9"
-                      name="input9"
+                      name="tx_mob_num"
                       placeholder="Enter Mobile Number"
                     />
                   </div>
@@ -145,8 +243,10 @@ const Taxi = () => {
                     </label>
                     <input
                       type="text"
+                      onChange={handleChange}
+                      style={{background:"#FFF",padding:"5px", fontWeight:"600" , border:"1px solid #FFA3A3"}}
                       id="input10"
-                      name="input10"
+                      name="tx_email"
                       placeholder="Enter Email"
                     />
                   </div>
@@ -157,8 +257,10 @@ const Taxi = () => {
                     </label>
 
                     <textarea
-                      style={{ height: "130px" }}
-                      name="input12"
+                    onChange={handleChange}
+                    style={{height:"130px",background:"#FFF",padding:"5px", fontWeight:"600" , border:"1px solid #FFA3A3"}}
+                     
+                      name="tx_comm"
                       id="input12"
                     ></textarea>
                   </div>
